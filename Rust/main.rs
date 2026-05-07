8cbbbd1dd3e1db7a3c4577d996939850ad6b98d9
Vaidulla <vajdullaak2@gmail.com>
2026-05-07 12:45:17 +0300

    Отправка отчета

---
+++ Rust/main.rs
@@ -0,0 +1,10 @@
+fn main (){
+    let number = 5;
+    if number > 0{
+        println!("Положительное число");
+    } else if number < 0{
+        println!("Отрицательное число");
+    } else {
+        println!("Ноль");
+    }
+}
